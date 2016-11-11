6ecd0f42d626c06298a106cf80249187eed206c6
Rahul Challapalli <challapallirahul@gmail.com>
2016-11-11 15:02:35 -0800

    Enabled the new parquet reader

--- before-run.sql
+++ before-run.sql
@@ -4,3 +4,4 @@ alter system set `planner.enable_decimal_data_type` = true;
 alter system set `new_view_default_permissions` = '777';
 alter system set `planner.enable_limit0_optimization` = true;
 alter system set `exec.errors.verbose` = true;
+alter session set `store.parquet.use_new_reader` = true;
