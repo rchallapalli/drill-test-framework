f27d19162bbeab5a09550744ff3e16d864ee8872
Rahul Challapalli <challapallirahul@gmail.com>
2016-11-23 11:01:15 -0800

    Enabled reading hive timestamps at the system level

--- after-run.sql
+++ after-run.sql
@@ -1,4 +1,5 @@
 alter system set `planner.enable_decimal_data_type` = false;
+alter system set `store.parquet.reader.int96_as_timestamp` = false;
 alter system set `new_view_default_permissions` = '700';
 alter system set `planner.enable_limit0_optimization` = false;
 alter system set `exec.errors.verbose` = false;
